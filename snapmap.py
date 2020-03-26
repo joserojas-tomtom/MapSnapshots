2765e0a6bede2024f93a297d7ec121a86ed18190
josejoserojas <49397824+josejoserojas@users.noreply.github.com>
2020-03-26 16:09:07 +0100

    Create snapmap.py

---
+++ snapmap.py
@@ -0,0 +1,11 @@
+# Code here!
+
+# Fetch parameters from the command line
+
+# Read the API KEY from the file somewhere
+
+# Create API CALL to 'static image'
+
+# Save image to a file ..
+
+# Check if image should be sent to email/attachment 
