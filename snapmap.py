bcad3250cec954430c85157cfb9d2098d9b159a9
Jose Jose Rojas <Jose.Rojas@tomtom.com>
2020-04-06 17:01:28 +0200

    Work in progress

--- snapmap.py
+++ snapmap.py
@@ -1,8 +1,28 @@
+import sys
 # Code here!
 
 # Fetch parameters from the command line
+# make sure there are 3 ..only 3
+# 4 because position 0 == the application name
+if (len(sys.argv) != 4): 
+  print ('I need 3 args!')
+  quit()
+
+#print ('Argument List:', str(sys.argv))
 
 # Read the API KEY from the file somewhere
+apiKey = sys.argv[3]
+zoom = sys.argv[2]
+city = sys.argv[1]
+
+# Get the center location of the city.
+# use this URL : https://api.tomtom.com/search/2/search/cancun.json?idxSet=Geo&key=*****
+url = 'https://api.tomtom.com/search/2/search/'+city+'.json?idxSet=Geo&key='+apiKey
+print ( url )
+
+req = urllib.request.urlopen(url)
+data = req.read().decode('utf-8')
+print(data)
 
 # Create API CALL to 'static image'
 
