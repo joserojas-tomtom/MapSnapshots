a897d3567896c14bcb65b6c2eda14fe99c0e77c1
Jose Jose Rojas <Jose.Rojas@tomtom.com>
2020-04-07 17:31:06 +0200

    getting the static image

--- snapmap.py
+++ snapmap.py
@@ -20,6 +20,8 @@ city = sys.argv[1]
 
 # Get the center location of the city.
 # use this URL : https://api.tomtom.com/search/2/search/cancun.json?idxSet=Geo&key=*****
+#
+# y4OUatPPGQgS71stpTvqD6n2iyteZrYD 
 url = 'https://api.tomtom.com/search/2/search/'+city+'.json?idxSet=Geo&key='+apiKey
 print ( url )
 
@@ -29,11 +31,30 @@ data = req.read().decode('utf-8')
 
 # Convert data into JSON
 obj = json.loads(data)
-print(obj["results"][0]["position"])
+
+if (len(obj["results"]) == 0):
+  print ('City not found')
+  quit()
+
+print('Found city center of '+city+' at '+ str(obj["results"][0]["position"]))
+print("...\n\n\n\n\n\n\n")
+lat = obj["results"][0]["position"]["lat"]
+lon = obj["results"][0]["position"]["lon"]
 
 
 # Create API CALL to 'static image'
-
+# https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&zoom=13&center=4.899886%2C%2052.379031&width=2048&height=2048&view=Unified&key=*****
+urlStaticImage = "https://api.tomtom.com/map/1/staticimage?" \
+                 "layer=basic&style=main&format=png&zoom="+zoom+"&" \
+                 "center="+str(lon)+","+str(lat)+"&width=2048&height=2048&key="+apiKey
+#print(urlStaticImage)
 # Save image to a file ..
+imageRequest = urllib.request.urlopen(urlStaticImage)
+imageBinary = imageRequest.read()
+sys.stdout.buffer.write(imageBinary)
+
+#small = 256x256
+#medium = 512x512
+#large= 1024x1024
 
 # Check if image should be sent to email/attachment 
