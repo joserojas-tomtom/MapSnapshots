1d0cd449c91368a203c9344563c538062593f9ca
Olivia-Christiana Vahsen <olivia-christiana.vahsen@tomtom.com>
2020-04-08 17:33:14 -0500

    Update snapmap.py

--- snapmap.py
+++ snapmap.py
@@ -5,18 +5,19 @@ import json
 # Code here!
 
 # Fetch parameters from the command line
-# make sure there are 3 ..only 3
-# 4 because position 0 == the application name
-if (len(sys.argv) != 4): 
-  print ('I need 3 args!')
+# make sure there are 4 ..only 4
+# 5 because position 0 == the application name
+if (len(sys.argv) != 5): 
+  print ('I need 4 args!')
   quit()
 
 #print ('Argument List:', str(sys.argv))
 
 # Read the API KEY from the file somewhere
-apiKey = sys.argv[3]
-zoom = sys.argv[2]
-city = sys.argv[1]
+apiKey = sys.argv[4]
+zoom = sys.argv[3]
+city = sys.argv[2]
+size = sys.argv[1]
 
 # Get the center location of the city.
 # use this URL : https://api.tomtom.com/search/2/search/cancun.json?idxSet=Geo&key=*****
@@ -41,12 +42,15 @@ print("...\n\n\n\n\n\n\n")
 lat = obj["results"][0]["position"]["lat"]
 lon = obj["results"][0]["position"]["lon"]
 
+small = '&width=256x&height=256'
+medium = '&width=512x&height=512'
+large= '&width=1024x&height=1024'
 
 # Create API CALL to 'static image'
 # https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&zoom=13&center=4.899886%2C%2052.379031&width=2048&height=2048&view=Unified&key=*****
 urlStaticImage = "https://api.tomtom.com/map/1/staticimage?" \
                  "layer=basic&style=main&format=png&zoom="+zoom+"&" \
-                 "center="+str(lon)+","+str(lat)+"&width=2048&height=2048&key="+apiKey
+                 "center="+str(lon)+","+str(lat)+str(size)"&key="+apiKey
 #print(urlStaticImage)
 # Save image to a file ..
 imageRequest = urllib.request.urlopen(urlStaticImage)
